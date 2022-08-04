e035cafeb26de7e4e09d58edaffe27c90fdd3106
thibaut lehmann <thiblehm12@gmail.com>
2022-08-04 13:53:46 +0200

    styling modal

--- components/modal/modal.tsx
+++ components/modal/modal.tsx
@@ -4,9 +4,13 @@ import Link from 'next/link'
 import { useContext, MutableRefObject, useRef, useEffect } from 'react'
 import styles from '../../styles/components/modal.module.css'
 import { ModalContext } from '../contexts/ModalContext'
-
+import { AiOutlineClose } from 'react-icons/ai'
 
 const Modal: NextPage = () => {
+    function capitalizeFirstLetter(string: string | undefined) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
     const { modalOpened, closeModal, modalProps } = useContext(ModalContext)
 
     const modalRef: MutableRefObject<any> = useRef()
@@ -30,10 +34,16 @@ const Modal: NextPage = () => {
     return modalOpened ? (
         <div className={styles.container} >
             <div className={styles.modal} ref={modalRef}>
+                <div className={styles.comingsoon}>COMING SOON ⌛</div>
+                <button className={styles.close} onClick={closeModal}><AiOutlineClose size={20} color={'#ccc'} /></button>
+                <div className={styles.user}>{capitalizeFirstLetter(modalProps?.name)},</div>
+                <div className={styles.description}>Join us before the launch and get an &nbsp;<br></br>
+                    <span className={styles.underline}>all time free</span> access.</div>
+                <div className={styles.buttons}>
 
-                user : {modalProps?.name}
+                </div>
+
             </div>
-            <button className={styles.close} onClick={closeModal}>close</button>
 
         </div>
     ) : null
